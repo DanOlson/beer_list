9be1bb5da7f5145d5bea23165d7d7ce55446ef41
Dan Olson <daniel.olson@code42.com>
2013-04-02 21:32:31 -0500

    Call the establishment directly on BeerList
    Example:
      BeerList.muddy_waters
      => "{\"BeerList::Establishments::MuddyWaters\":[\"\",\"Surly  Bend...

--- lib/beer_list.rb
+++ lib/beer_list.rb
@@ -10,5 +10,11 @@ module BeerList
     def scraper
       @scraper ||= Scraper.new
     end
+
+    def method_missing(method, *args, &block)
+      est = method.to_s.split('_').map(&:capitalize).join
+      klass = ['BeerList', 'Establishments', est].compact.inject(Object){ |o, name| o.const_get(name) }
+      scraper.beer_list klass.new
+    end
   end
 end
