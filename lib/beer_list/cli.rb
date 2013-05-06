ac47b442b88f115485be3663e2b23c738767f676
Dan Olson <daniel.olson@code42.com>
2013-05-05 20:11:08 -0500

    Use Thor for the CLI

--- lib/beer_list/cli.rb
+++ lib/beer_list/cli.rb
@@ -1,65 +1,23 @@
-require 'optparse'
+require 'thor'
 
 module BeerList
-  class CLI
-    COMMANDS = %w(establish)
+  class CLI < Thor
+    option :url,
+           aliases: '-u',
+           banner:  "url of your establishment's beer list"
+    option :directory,
+           required: true,
+           aliases:  '-d',
+           banner:   'the directory in which BeerList will put your establishments'
+    option :selector,
+           aliases: '-s',
+           banner: 'selector to use for scraping'
 
-    class << self
-      def start(args)
-        options = parse ARGV
-
-        msg = ARGV[0]
-        abort unsupported_command unless COMMANDS.include? msg
-
-        new(options).public_send msg
-      end
-
-      def parse(args)
-        options = {}
-
-        opts = OptionParser.new do |opts|
-          opts.banner = "Usage: beer_list establish [options]"
-
-          opts.on("-u", "--url [URL]", "url of your establishment's beer list") do |url|
-            options[:url] = url
-          end
-
-          opts.on("-s", "--selector [SELECTOR]", "selector to use for scraping") do |s|
-            options[:selector] = s
-          end
-
-          opts.on("-d", "--directory DIR", "the directory in which BeerList will put your establishments") do |dir|
-            options[:dir] = dir
-          end
-        end
-
-        opts.parse! args
-        options
-      end
-
-      def unsupported_command
-        <<-WARN
-        Command: not supported. Must be one of the following:
-        #{COMMANDS.join(', ')}
-        WARN
-      end
-
-      def no_establishments_dir
-        <<-WARN
-        You must supply an establishments directory with -d or --directory
-        WARN
-      end
-    end
-
-    def initialize(args)
-      @args = args
-    end
-
-    def establish(klass=ARGV[1])
-      abort self.class.no_establishments_dir unless @args[:dir]
+    desc 'establish ESTABLISHMENT', 'Generates a subclass of BeerList::Establishments::Establishment in the given directory'
+    def establish(klass)
       # Support underscore and camelcase
       klass = klass.split('_').map(&:capitalize).join if klass.match(/_/)
-      BeerList::EstablishmentGenerator.new(klass, @args)
+      BeerList::EstablishmentGenerator.new(klass, options)
     end
   end
 end
